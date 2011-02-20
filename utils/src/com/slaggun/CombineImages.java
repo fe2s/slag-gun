6efd73d4ef52fd8d2041f205d3dd7ab1745524e7
Dmitry Brazhnik <amid.ukr@gmail.com>
2011-02-20 09:51:42 +0200

    Combine images util.

---
+++ utils/src/com/slaggun/CombineImages.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2010 SlagGunTeam
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language governing permissions
+ * and limitations under the License.
+ */
+
+package com.slaggun;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+import java.io.FilenameFilter;
+import java.util.Arrays;
+
+/**
+ * @author Dmitry Brazhnik (amid.ukr@gmail.com)
+ */
+public class CombineImages {
+	public static void main(String[] args) throws IOException {
+
+		File dir = new File("C:/development/stalker/out/top2/frames");
+		File[] images = dir.listFiles(new FilenameFilter() {
+			public boolean accept(File dir, String name) {
+				return name.endsWith(".png");
+			}
+		});
+		Arrays.sort(images);
+
+		BufferedImage result = new BufferedImage(
+                               images.length * 104, 833, //work these out
+                               BufferedImage.TYPE_INT_ARGB);
+        Graphics g = result.getGraphics();
+
+
+
+		int x = 0;
+
+		System.out.println("----------");
+		for(File image : images){
+			System.out.println("Reading: " + image.getPath());
+            BufferedImage bi = ImageIO.read(image);
+            g.drawImage(bi, x, 0, null);
+            x += 104;
+        }
+		System.out.println("----------");
+
+
+
+		ImageIO.write(result, "png", new File("C:/development/slag-gun/trunk/hg/client/src/com/slaggun/actor/player/renderer/res/stalker.png"));
+		//ImageIO.write(result, "png", new File("C:/development/stalker/out/top2/temp.png"));
+
+		System.out.println("Done");
+	}
+}
